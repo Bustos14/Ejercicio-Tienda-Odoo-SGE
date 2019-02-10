77c782b0090786d9a717a19f3e06e2329788a557
Bustos14 <33839835+Bustos14@users.noreply.github.com>
2019-02-10 11:22:28 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Plugin Tienda',
+    'description': 'Modelo de una tienda basico',
+    'category': 'Point Of Sale',
+    'summary' : 'APP tienda cliente/articulo/proveedor/venta',
+    'author': 'Alvaro Bustos Gonzalez',
+    'depends': ['mail'],
+    'application': True,
+    'data': ['views/proveedores.xml', 'views/clientes.xml', 'views/articulos.xml', 'views/ventas.xml']
+}
